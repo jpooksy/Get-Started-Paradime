93792c5c2835845b5958dcf031630cb89f72894c
Parker Rogers <parker@paradime.io>
2024-09-06 06:00:01 +0000

    Refactor SQL for readability by condensing CTEs in intermediate_player_info.sql

--- models/Intermediate/intermediate_player_info.sql
+++ models/Intermediate/intermediate_player_info.sql
@@ -1,33 +1,6 @@
-WITH player_info AS (
-    SELECT 
-        *
-    FROM 
-        {{ ref('nba_player_info') }}
-),
-
-player_salaries AS (
-    SELECT 
-        player_id, 
-        salary, 
-        season
-    FROM 
-        {{ ref('nba_player_salaries') }}
-), 
-
-joined AS (
-
-SELECT 
-    pi.*,
-    ps.salary,
-    ps.season
-FROM 
-    player_info pi
-LEFT JOIN 
-    player_salaries ps ON pi.player_id = ps.player_id
-)
-
-
-SELECT 
-    * 
-FROM 
-    joined
+WITH player_info AS (SELECT * FROM {{ ref('nba_player_info') }})
+, player_salaries AS (SELECT player_id, salary
+, season FROM {{ ref('nba_player_salaries') }})
+, joined AS (SELECT pi.*, ps.salary, ps.season FROM player_info AS pi
+LEFT JOIN player_salaries AS ps ON pi.player_id = ps.player_id)
+SELECT * FROM joined
