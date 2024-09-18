d95fd621a7ee17387fc221e1b0eefc66895312cb
Parker Rogers <parker@paradime.io>
2024-09-18 20:07:07 +0000

    Refactor SQL query for better readability and correct join alias.

--- models/Intermediate/intermediate_player_info.sql
+++ models/Intermediate/intermediate_player_info.sql
@@ -1,9 +1,29 @@
-with player_info as (     
-select * from {{ ref('nba_player_info') }} )
+with player_info as (
+    select *
+    from
+        {{ ref('nba_player_info') }}
+),
 
-, player_salaries as (     
-select player_id, salary, season from {{ ref('nba_player_salaries') }} ),  
+player_salaries as (
+    select
+        player_id,
+        salary,
+        season
+    from
+        {{ ref('nba_player_salaries') }}
+),
 
 joined as (
-select pi.*, ps.salary,ps.season from player_info as pi
-left join player_salaries as ps on pi.player_id = pa.player_id )
+
+    select
+        pi.*,
+        ps.salary,
+        ps.season
+    from
+        player_info as pi
+    left join
+        player_salaries as ps
+        on pi.player_id = ps.player_id
+)
+
+select * from joined
